f4ccef4c01999a74559750f3c76ff097fc8e8810
murffious <titan.murff@gmail.com>
2019-08-17 13:49:00 -0600

    working on rdnd

--- src/components/FavDragOrder.js
+++ src/components/FavDragOrder.js
@@ -1,6 +1,6 @@
 import React, { useImperativeHandle, useRef } from 'react'
 import { DragSource, DropTarget } from 'react-dnd'
-import ItemTypes from './ItemTypes'
+import ItemTypes from './helpers/ItemTypes'
 const style = {
   border: '1px dashed gray',
   padding: '0.5rem 1rem',
