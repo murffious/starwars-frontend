82d93d7f1c30d9f8eaddd54f0c472134e5349ec2
murffious <titan.murff@gmail.com>
2019-08-16 19:04:12 -0600

    data fetched and put on UI in list

---
+++ src/components/StarWars.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState, Fragment } from "react";
+
+
+function getPeople(){
+ return fetch('https://swapi.co/api/people')
+ .then(data => data.json());
+}
+export default function StarWarsPeopleList(){
+ const [peopleList, setPeopleList] = useState(null);
+    useEffect(()=>{
+        getPeople()
+        .then(results => {
+            console.log(results);
+            return results.results.map(
+                ({name, birth_year, homeworld, url}) =>{
+                    return { name, birth_year, homeworld, url };
+                }
+            )
+        }).then(peopleList => setPeopleList(peopleList))
+        .catch(err => setPeopleList([]))
+    }, [])
+    console.log(peopleList)
+    return (
+        <div>
+          {peopleList === null ? "...loading" : peopleList.length > 0 ?(
+       <table> 
+           <tbody>
+           <tr>
+            <th>Name</th>
+            <th>Homeworld</th>
+            <th>Birthday</th>
+            </tr>
+           {peopleList.map(person => {
+               return (<Fragment key={person.url}>
+                <tr>
+                    <td>{person.name}</td>
+                    <td>{person.homeworld}</td>
+                    <td>{person.birth_year}</td>
+               </tr>
+               </Fragment>);
+           })}
+           </tbody>
+           </table>): "error"
+        }
+        </div>
+        
+    )
+}
