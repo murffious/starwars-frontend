99e51718c5a27c72d82e066e75e77248a791a39e
murffious <titan.murff@gmail.com>
2019-08-17 13:46:07 -0600

    card boiler code adding

--- src/components/FavsListContainer.js
+++ src/components/FavsListContainer.js
@@ -7,48 +7,18 @@ const style = {
 }
 const Container = (props) => {
   {
-    const [favorites, setFavorites] = useState([
-      {
-        id: 1,
-        text: 'Write a cool JS library',
-      },
-      {
-        id: 2,
-        text: 'Make it generic enough',
-      },
-      {
-        id: 3,
-        text: 'Write README',
-      },
-      {
-        id: 4,
-        text: 'Create some examples',
-      },
-      {
-        id: 5,
-        text:
-          'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
-      },
-      {
-        id: 6,
-        text: '???',
-      },
-      {
-        id: 7,
-        text: 'PROFIT',
-      },
-    ])
+    const [favorites, setFavorites] = useState(props.favorites)
     const moveCard = (dragIndex, hoverIndex) => {
-      const dragCard = cards[dragIndex]
-      setCards(
-        update(cards, {
+      const dragCard = favorites[dragIndex]
+      setFavorites(
+        update(favorites, {
           $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
         }),
       )
     }
     return (
       <div style={style}>
-        {cards.map((card, i) => (
+        {favorites.map((card, i) => (
           <FavList
             key={card.id}
             index={i}
