bda4b29ef51bb486412b69f8eca7f5d0292f4662
murffious <titan.murff@gmail.com>
2019-08-16 21:30:32 -0600

    going to grab a reference obj from react docs

--- src/components/StarWars.js
+++ src/components/StarWars.js
@@ -3,13 +3,18 @@ import React, { useEffect, useState, Fragment } from "react";
 function getPeople() {
   return fetch("https://swapi.co/api/people").then(data => data.json());
 }
+function getPage(pageDataObj){
+  const [pageKeys, setKeys] = useState(null);
+    setKeys(pageDataObj)
+}
 export default function StarWarsPeopleList() {
   const [peopleList, setPeopleList] = useState(null);
   useEffect(() => {
     getPeople()
       .then(results => {
-        // console.log(results);
-        // looks like I need to go get the world from given url
+        // console.log(results);\
+         getPage({ count: results.count, previous: results.previous, next: results.next })
+               // looks like I need to go get the world from given url
         return results.results.map(({ name, birth_year, homeworld, url }) => {
           return { name, birth_year, homeworld, url };
         });
