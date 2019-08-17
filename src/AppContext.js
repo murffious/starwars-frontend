6d8337d32520a50a55b27b5cc1f0ec9efc02cdc1
murffious <titan.murff@gmail.com>
2019-08-17 11:55:11 -0600

    reusing some code added

--- src/AppContext.js
+++ src/AppContext.js
@@ -20,9 +20,30 @@ export class AppContextProvider extends Component {
 routeToFaves = () => {
 
 }
+async addPlayer(){
+  let length = this.state.players.length;
+
+  let players = this.state.players.concat([
+      { 
+          name: `${this.props.organization.name} ${length + 1}`, 
+          orientation: 'horizontal' 
+      }
+  ]);
+ 
+  this.setState({ 
+      players: players
+  });
+}
+
+removePlayer(index) {
+  let players = this.state.players;
+  players.splice(index, 1);
+  
+  this.setState({players})
+}
 hanldeLike = (toggle) => {
   let updateFavs = [...this.favorites];
-  
+
   this.setState({
     likesCount: toggle? this.state.likesCount+1: this.state.likesCount-1,
     favorite: updateFavs
