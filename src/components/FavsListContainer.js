88e125caec59d67525ebb8b21b826cbd2c5c4513
murffious <titan.murff@gmail.com>
2019-08-17 13:34:51 -0600

    going to docs of RDnD real fast

--- src/components/FavsListContainer.js
+++ src/components/FavsListContainer.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import FavList from "./FavList";
 import { withContext } from "../AppContext";
-
+// going to docs of RDnD real fast 
 class FavsListContainer extends React.Component {
   constructor() {
     super();
